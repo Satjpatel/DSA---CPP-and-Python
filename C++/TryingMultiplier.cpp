ab9b6876783942699c20cf5d0151c4aeb879d901
Satjpatel <44218342+Satjpatel@users.noreply.github.com>
2020-12-31 05:33:02 +0530

    Trying a new version of shift and Add Multiplier

---
+++ C++/TryingMultiplier.cpp
@@ -0,0 +1,21 @@
+#include "iostream" 
+using namespace std ; 
+
+void main() 
+{ 
+    int Multiplier = { 1 , 1 , 1} ; 
+    int Multiplicand = { 1, 0, 1} ; 
+    int Product [7] ;  
+
+     NewFunction ( )
+}
+
+int NewFunction ( int Multiplier[], int Multiplicand[], int Product [] ) 
+{ 
+    int CheckBit = 0 ; 
+
+    
+
+
+
+}
